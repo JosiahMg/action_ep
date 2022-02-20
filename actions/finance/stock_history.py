2dd7d8ea83067d37c60de6448facaae62425264b
xiazhou <xiazhou@fudata.cn>
2022-02-20 22:04:03 +0800

    update files

---
+++ actions/finance/stock_history.py
@@ -0,0 +1,16 @@
+import json
+import os
+from typing import Any
+
+import pandas as pd
+from actions.api.nowapi import NowApi
+from actions.constants import ACTIONS_PATH
+from actions.utils.create_log import logger
+
+
+class StockHistory:
+    def __init__(self):
+        # api定义
+        self.api = NowApi()
+        self.api_params = self.api.params
+        self.api_params["app"] = "finance.stock_history"
